6bad32031f144ae072b68cb73ba9d03be5c1fb33
Andrei Tiurin <xxxateoxxc@gmail.com>
2024-07-26 23:09:40 +0300

    renew

--- conftest.py
+++ conftest.py
@@ -2,18 +2,14 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-import pytest
-from selenium import webdriver
-
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default='en',
+    parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-
+    parser.addoption('--language', action='store', default="en")
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         browser = webdriver.Chrome()
@@ -21,7 +17,7 @@ def browser(request):
         print("\nstart firefox browser for test..")
         browser = webdriver.Firefox()
     else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        print("Browser {} still is not implemented".format(browser_name))
     yield browser
     print("\nquit browser..")
     browser.quit()
