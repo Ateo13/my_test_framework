82eb2ef1a8f4f79d73d9e4e0a92730e93f246b01
Andrei Tiurin <xxxateoxxc@gmail.com>
2024-07-29 22:32:46 +0300

    new methods

--- pages/locators.py
+++ pages/locators.py
@@ -6,5 +6,14 @@ class MainPageLocators():
 
 
 class LoginPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    CART_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    MAIN_BOOK_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    ALERT_BOOK_NAME = (By.CSS_SELECTOR, ".alertinner strong")
+    BOOK_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    BASKET_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
